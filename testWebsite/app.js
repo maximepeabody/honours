84cb2026a801b13a129b4411d436744c121b0cd5
unknown <maximepeabody@WIN8-4115-10.windows.scs.carleton.ca>
2016-03-15 21:22:27 -0400

    testwebsite working

--- testWebsite/app.js
+++ testWebsite/app.js
@@ -16,6 +16,17 @@ angular.module('app', ['google.places', 'ngResource'])
 			ride.route.duration = leg.duration;
 			ride.route.duration_in_traffic = leg.duration_in_traffic;
 			ride.route.polyline = directions.routes[0].overview_polyline;
+			var bounds = directions.routes[0].bounds;
+			ride.route.bounds = { 
+				northeast: {
+					lat:bounds.getNorthEast().lat(),
+					lng:bounds.getNorthEast().lng()
+				},
+				southwest: {
+					lat:bounds.getSouthWest().lat(),
+					lng:bounds.getSouthWest().lng()
+				}
+			};
 
 			console.log(ride);
 			return ride;
@@ -55,8 +66,9 @@ angular.module('app', ['google.places', 'ngResource'])
 
 .controller('searchCtrl', function($scope, $resource) {
 	var rideDbs = $resource('http://45.55.157.150:8080/ride');
+			var directionsService = new google.maps.DirectionsService;
+
 	$scope.search = function(query) {
-		console.log(query);
 		var q = {
 			originlat : query.origin.geometry.location.lat(),
 			originlng : query.origin.geometry.location.lng(),
@@ -65,10 +77,10 @@ angular.module('app', ['google.places', 'ngResource'])
 			date :query.date, 
 			type: query.type
 		};
-		console.log(q);
-		//console.log(rideDbs.query(q));
+
 		$scope.searchResults = rideDbs.query(q);
 		console.log($scope.searchResults);
+
 	};
 })
 
